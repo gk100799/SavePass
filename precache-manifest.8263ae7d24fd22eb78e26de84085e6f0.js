9c1b5f0cf4fbd495faed6587427aaf8760bb5384
gk100799 <55948183+gk100799@users.noreply.github.com>
2020-09-07 00:46:27 +0530

    Updates

---
+++ precache-manifest.8263ae7d24fd22eb78e26de84085e6f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb24785e240983d2df681db9d87346e",
+    "url": "/SavePass/index.html"
+  },
+  {
+    "revision": "feb0f4ccd635538f4c73",
+    "url": "/SavePass/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "b6dcb4899f4861b23fa9",
+    "url": "/SavePass/static/css/main.f472f10f.chunk.css"
+  },
+  {
+    "revision": "feb0f4ccd635538f4c73",
+    "url": "/SavePass/static/js/2.8ddb1aa0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/SavePass/static/js/2.8ddb1aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6dcb4899f4861b23fa9",
+    "url": "/SavePass/static/js/main.4e5ff0f2.chunk.js"
+  },
+  {
+    "revision": "1fae4c54f5a7eb10ce04",
+    "url": "/SavePass/static/js/runtime-main.d631ac19.js"
+  }
+]);
